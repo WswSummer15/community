8b73c6674c1a7ca4afd2a229a29c44bfb60ef0f3
WSW <2544894086@qq.com>
2019-08-29 13:17:33 +0800

    新增函数

--- src/main/java/com/wsw/community/controller/AuthorizeController.java
+++ src/main/java/com/wsw/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * 认证
  */
@@ -29,7 +31,8 @@ public class AuthorizeController {
     private String redirectUrl;
 
     @RequestMapping("/callback")
-    public String callback(@RequestParam("code") String code, @RequestParam("state") String state){
+    public String callback(@RequestParam("code") String code,
+                           @RequestParam("state") String state, HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -38,9 +41,13 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        System.out.println(user.getId());
-        System.out.println(user.getBio());
-        return "index";
+        if (user != null){
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
